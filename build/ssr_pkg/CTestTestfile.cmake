48ea2e465afaadb92e3ce90e5d7ee49eef524970
gdm <109819399+1875525594@users.noreply.github.com>
2023-07-07 17:13:34 +0800

    Delete build directory
    1

--- build/ssr_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gdm0/work_code/src/ssr_pkg
-# Build directory: /home/gdm0/work_code/build/ssr_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
