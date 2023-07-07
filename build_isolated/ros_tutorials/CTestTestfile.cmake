f7ecd25ebdd538b458edc1e0fc3c7a3beac882ce
gdm <109819399+1875525594@users.noreply.github.com>
2023-07-07 17:14:43 +0800

    Delete build_isolated directory
    1

--- build_isolated/ros_tutorials/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gdm0/work_code/src/ros_tutorials/ros_tutorials
-# Build directory: /home/gdm0/work_code/build_isolated/ros_tutorials
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- build_isolated/wpr_simulation/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gdm0/work_code/src/wpr_simulation
-# Build directory: /home/gdm0/work_code/build_isolated/wpr_simulation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
